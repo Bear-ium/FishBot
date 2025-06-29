06ad4bcebf2883b48944fa1666e29790ae9ae727
Evie <eviecam2006@gmail.com>
2025-06-29 20:18:50 +0100

    OAuth Web Beautified

--- Auth/app.py
+++ Auth/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, render_template, url_for
 from dotenv import load_dotenv
 import requests
 import os
 
 load_dotenv()
-
 
 # Important Variables. Probably don't touch them..
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -18,10 +17,7 @@ app.secret_key = os.urandom(24)  # Generate a random secret key for securely sig
 
 @app.route('/')
 def home():
-    return '''
-    <h1>Twitch OAuth Login<h1>
-    <a href="/login">Log in with Twitch</a>
-    '''
+    return render_template('home.html')
 
 @app.route('/login')
 def login():
@@ -38,7 +34,7 @@ def callback():
     code = request.args.get('code')
     if not code:
         return 'No code provided', 400
-    
+
     tokenUrl = 'https://id.twitch.tv/oauth2/token'
     data = {
         'client_id': CLIENT_ID,
@@ -47,7 +43,7 @@ def callback():
         'grant_type': 'authorization_code',
         'redirect_uri': REDIRECT_URI,
     }
-    
+
     response = requests.post(tokenUrl, data=data)
     if response.status_code != 200:
         return f"Error getting token: {response.text}", 500
@@ -55,11 +51,7 @@ def callback():
     tokenInfo = response.json()
     accessToken = tokenInfo.get('access_token')
 
-    return f'''
-        <h2>OAuth Token Received</h2>
-        <p><b>Access Token:</b> {accessToken}</p>
-        <p>Copy this and use it in your bot code as <code>oauth:{accessToken}</code></p>
-    '''
+    return render_template('callback.html', access_token=accessToken)
 
 if __name__ == '__main__':
     app.run(port=8080)
